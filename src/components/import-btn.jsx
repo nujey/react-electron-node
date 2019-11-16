7e665a062cb620ec571b5a0aa2f6a5a3161413fa
张锋 <zhangfeng930101@qq.com>
2019-11-16 16:25:47 +0800

    s

--- src/components/import-btn.jsx
+++ src/components/import-btn.jsx
@@ -6,47 +6,35 @@ import XLSX from 'xlsx'
 function ImportBtn(props) {
   const [mapList, setMapList] = useState([])
   const handleImport = (e) => {
-    let self = this
-    let files = e.target.files
-    for (let i = 0, f = files[i]; i !== files.length; ++i) {
-      let reader = new FileReader()
-      self.fileName = f.name
-      reader.onload = function (e) {
-        let data = e.target.result
+    // console.log(e.target)
+    let { files } = e.target
 
-        let workbook = XLSX.read(data, {
-          type: 'binary'
-        })
-        // 取第一个SheetNames标签workbook.SheetNames[0]
-        let xlsxSrc = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]])
-        let content = []
-
-        // 复制一份防止excel文件过来的文件有空字段这个属性就读出不来
-        let list = {}
-
-        xlsxSrc.map(function (item) {
-          for (let attr in self.mapList) {
-            list[self.mapList[attr]] = ''
+    const fileReader = new FileReader()
+    fileReader.onload = event => {
+      console.log(event)
+      try {
+        const { result } = event.target
+        const workbook = XLSX.read(result, { type: 'binary' })
+        let data = []
+        for (const sheet in workbook.Sheets) {
+          if (workbook.Sheets.hasOwnProperty(sheet)) {
+            data = data.concat(XLSX.utils.sheet_add_json(workbook.Sheets[sheet]))
           }
-          for (let attr in item) {
-            list[self.mapList[attr]] = item[attr] // 去除空格
-          }
-          const obj = JSON.parse(JSON.stringify(list))
-          content.push(obj)
-
-          // $(self).val(''); //解决同文件上传问题
-        })
+        }
+        console.log(data)
+      } catch(e) {
+        console.log(e, 'catch')
       }
-      reader.readAsBinaryString(f)
     }
+    fileReader.readAsBinaryString(files[0])
   }
   const accept = ".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
   return(
     <>
       <label htmlFor="fileSelect">
-        <Button type="primary" ><Icon type="left" /> 导入</Button>
+        <span>导入</span>
       </label>
-      <input onChange={handleImport} style={{display: 'none'}} type="file" name="fileSelect" id="fileSelect" accept={accept}/>
+      <input onChange={(e) => { handleImport(e) } } style={{display: 'none'}} type="file" name="fileSelect" id="fileSelect" accept={accept}/>
     </>
   )
 }
