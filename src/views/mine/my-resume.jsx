e1e57cb0cbc760f9177e5736ca94a4b67f0d331b
张锋 <zhangfeng930101@qq.com>
2020-01-02 18:12:27 +0800

    resume简历

--- src/views/mine/my-resume.jsx
+++ src/views/mine/my-resume.jsx
@@ -13,7 +13,7 @@ function MyResumeFrom(props) {
     <div className="my-resume">
       <div className="resume-buttons">
         <Button type="dashed" icon="left">返回</Button>
-        <Button type="dashed" icon="edit">编辑</Button>
+        <Button type="dashed" icon="form">编辑</Button>
       </div>
       <div id="main-resume" className="main-resume">
         <section>
@@ -21,51 +21,70 @@ function MyResumeFrom(props) {
             <span>基础信息</span>
             <Button type="dashed" icon="save">保存</Button>
           </div>
-          <Form>
-            <Form.Item label="姓名">
-              {getFieldDecorator('name', {
-                rules: [{ required: true, message: '请输入你的姓名' }]
-              })(
-                <Input  prefix={<Icon type="user"/>} placeholder="姓名"/>
-              )}
-            </Form.Item>
-            <Form.Item label="年龄">
-              {getFieldDecorator('age', {
-                rules: [{ required: true, message: '请输入你的年龄' }]
-              })(
-                <Input  prefix={<Icon type="age"/>} placeholder="年龄"/>
-              )}
-            </Form.Item>
-            <Form.Item label="联系方式">
+          <Form className="resume-basic-form">
+            <div className="resume-basic-left">
+              <Form.Item label="姓名">
+                {getFieldDecorator('name', {
+                  rules: [{ required: true, message: '请输入你的姓名' }]
+                })(
+                  <Input  prefix={<Icon type="user"/>} placeholder="姓名"/>
+                )}
+              </Form.Item>
+              <Form.Item label="年龄">
+                {getFieldDecorator('age', {
+                  rules: [{ required: true, message: '请输入你的年龄' }]
+                })(
+                  <Input  prefix={<Icon type="heart"/>} placeholder="年龄"/>
+                )}
+              </Form.Item>
+              <Form.Item label="性别">
+                {getFieldDecorator('sex', {
+                  rules: [{ required: true, message: '请选择性别' }]
+                })(
+                  <Input  prefix={<Icon type="woman"/>} placeholder=""/>
+                )}
+              </Form.Item>
+              <Form.Item label="籍贯">
+                {getFieldDecorator('home', {
+                  rules: [{ required: true, message: '请输入你的籍贯' }]
+                })(
+                  <Input  prefix={<Icon type="home"/>} placeholder="籍贯"/>
+                )}
+              </Form.Item>
+            </div>
+            <div className="resume-basic-photo">
+
+            </div>
+            <Form.Item label="联系方式" style={{width: '48%', marginRight: '4%'}}>
               {getFieldDecorator('mobile', {
                 rules: [{ required: true, message: '请输入你的手机号码' }]
               })(
-                <Input  prefix={<Icon type="user"/>} placeholder="手机号码"/>
+                <Input  prefix={<Icon type="mobile"/>} placeholder="手机号码"/>
               )}
             </Form.Item>
-            <Form.Item label="籍贯">
-              {getFieldDecorator('home', {
-                rules: [{ required: true, message: '请输入你的籍贯' }]
+            <Form.Item label="现居住地" style={{width: '48%'}}>
+              {getFieldDecorator('address', {
+                rules: [{ required: true, message: '请输入你的现居住地' }]
               })(
-                <Input  prefix={<Icon type="user"/>} placeholder="籍贯"/>
-              )}
-            </Form.Item>
-            <Form.Item label="">
-              {getFieldDecorator('name', {
-                rules: [{ required: true, message: '请输入你的姓名' }]
-              })(
-                <Input  prefix={<Icon type="user"/>} placeholder="姓名"/>
+                <Input  prefix={<Icon type="barcode"/>} placeholder="居住地址"/>
               )}
             </Form.Item>
           </Form>
-          {/* <div className="resume-basic">
+        </section>
+        
+        <section>
+          <div className="resume-item-title">
+            <span>项目经验</span>
+            <Button type="dashed" icon="save">保存</Button>
+          </div>
+          <div className="resume-project-add">
+            <Icon type="plus" style={{fontSize: 80, color: '#38adff'}} ></Icon>
+          </div>
+          {/* <Form className="resume-basic-form">
             <div className="resume-basic-left">
-
+              
             </div>
-            <div className="resume-basic-right">
-
-            </div>
-          </div> */}
+          </Form> */}
         </section>
       </div>
     </div>
