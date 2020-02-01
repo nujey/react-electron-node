483ce62825eac3fc1ae180c9d99a451380a4b62e
张锋 <zhangfeng930101@qq.com>
2020-02-01 21:11:09 +0800

    context 上下文

--- src/views/test-study/context.jsx
+++ src/views/test-study/context.jsx
@@ -9,43 +9,67 @@ const themes = {
   dark: {
     foreground: '#fff',
     background: '#222'
+  },
+  user: {
+    name: 'user'
   }
 }
 
-const ThemeContext = React.createContext(themes.dark)
+const ThemeContext = React.createContext({
+  theme: themes.dark,
+  toggleTheme: () => {}
+})
+const UserContext = React.createContext({ name: 'Guest' })
 
 class Study extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      theme: themes.light
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme: this.state.theme === themes.dark ? themes.light : themes.dark
+      }))
     }
-  }
-  toggleTheme = () => {
-    this.setState(state => ({
-      theme: this.state.theme === themes.dark ? themes.light : themes.dark
-    }))
-  }
-  componentDidMount() {
-    console.log(this.state.theme)
+    this.state = {
+      theme: themes.light,
+      toggleTheme: this.toggleTheme
+    }
   }
   render() {
     return (
       <>
         <h3>context</h3>
-        <ThemeContext.Provider value={this.state.theme}>
+        <ThemeContext.Provider value={this.state}>
           <ThemeBtn changeTheme={this.toggleTheme} />
+          <UserContext.Provider value={themes.user}>
+            <Layout />
+          </UserContext.Provider>
+          <ThemeTogglerButton />
         </ThemeContext.Provider>
         <section>
           {/* <BasicBtn /> */}
+          <ThemeTogglerButton />
         </section>
       </>
     )
   }
 }
 
+function ThemeTogglerButton(props) {
+  return (
+    <ThemeContext.Consumer>
+      {({theme, toggleTheme}) => {
+        return <button
+          onClick={toggleTheme}
+          style={{backgroundColor: theme.background, color: '#f00'}}>
+          Toggle Theme
+        </button>
+        }
+      }
+    </ThemeContext.Consumer>
+  )
+}
+
 function ThemeBtn(props) {
-  console.log(props)
   return (
     <>
       <BasicBtn onClick={props.changeTheme}>改变主题</BasicBtn>
@@ -59,9 +83,26 @@ class BasicBtn extends React.Component {
   render() {
     let props = this.props
     let theme = this.context
-    console.log(theme, 111)
-    return <Button {...props} style={{backgroundColor: theme.background}}></Button>
+    return <Button {...props} style={{backgroundColor: theme.theme.background}}></Button>
   }
 }
 
+
+function Layout() {
+  return (
+    <div>
+      <Content />
+    </div>
+  )
+}
+function Content(props) {
+  return (
+    <UserContext.Consumer>
+      {(user) => {
+        return <span user={user.name}>{user.name}</span>
+        }
+      }
+    </UserContext.Consumer>
+  )
+}
 export default Study
